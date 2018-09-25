89c73234ef917391c5434caac12934a867f88f39
Sydney Grant <sg3957@nyu.edu>
2018-09-25 04:52:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(num)
  if num % 3 == 0 && num % 5 == 0
-  puts "FizzBuzz"
+  return "FizzBuzz"
 elsif num % 3 == 0
-  puts "Fizz"
+  return "Fizz"
 elseif num % 5 == 0
-  puts "Buzz"
+  return "Buzz"
 else
-  puts nil
+  return nil
 
 end
